8036ff50f6d074bc4bc101aadab735ef54632b16
Nilesh Gautam <gautamnilesh084@gmail.com>
2022-08-08 14:51:10 +0530

    Updates

---
+++ precache-manifest.2fe5adf89c655a4cb3ffbaff369a885d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee8989663aa2eaae712f",
+    "url": "/first-react/static/css/main.070ddda9.chunk.css"
+  },
+  {
+    "revision": "ee8989663aa2eaae712f",
+    "url": "/first-react/static/js/main.ab1836e2.chunk.js"
+  },
+  {
+    "revision": "d552759458c0c008aea7",
+    "url": "/first-react/static/js/runtime~main.8a7bbe4f.js"
+  },
+  {
+    "revision": "58493f84ea3b13214720",
+    "url": "/first-react/static/js/2.3f9b2d3a.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/first-react/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "04bda984281b2cc4ea85e9a700f0a6cf",
+    "url": "/first-react/index.html"
+  }
+];
